84355d9cd8963bea7cb3dd351c0e2b902207716d
Toby Cole <tub@tubs.org.uk>
2012-10-12 09:52:48 +0100

    Fixing herobox nav overflow

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(window).load(function() {
      // side bar
     $('#navbar').affix({
       offset: {
-        top: function () { return $(window).width() <= 980 ? 155 : 225 }
+        top: function () { return $(window).width() <= 980 ? 474 : 522 }
       , bottom: 270
       }
     })
